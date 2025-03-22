cb299b423e08a8d73ee55162e0cf92d0f2191de2
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-03-22 23:07:37 +0530

    Admin details

--- Frontend/src/components/Register/SignUp/Admin.jsx
+++ Frontend/src/components/Register/SignUp/Admin.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Signupnav from './Signupnav'
 const Admin = () => {
+  const [date, setDate] = useState("");
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <Signupnav/>
       <h1>Admin</h1>
-      
+      <div className="heading text-center mb-5 ">
+        <h1 className="font-bold text-4xl">Sign Up</h1>
+      </div>
+
+      <div className="name mb-5">
+        <input type="text"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='First Name' />
+        <input type="text"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Last Name' />
+      </div>
+
+      <div className="details mb-5">
+        <input type="email"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Email' />
+        <input type="number"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Mobile Number' />
+      </div>
+
+      <div className="NIC mb-5">
+        <input
+          type="number"
+          className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4"
+          placeholder="Adhar Card Number"
+        />
+        <div className="relative inline-block">
+          <input
+            className="border-4 border-gray-500 text-gray-500 rounded-lg p-2 w-100 appearance-none"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          {!date && (
+            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none bg-white px-2">
+              Date of Birth
+            </span>
+          )}
+        </div>
+      </div>
+
+      <div className="password mb-5">
+        <select className="border-4 border-gray-500 rounded-lg p-2 w-60 mr-12">
+          <option value="Gender">Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+
+        <input
+          type="password"
+          className="border-4 border-gray-500 rounded-lg p-2 w-60 mr-12"
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          className="border-4 border-gray-500 rounded-lg p-2 w-60 "
+          placeholder="Confirm Password"
+        />
+      </div>
+
+      <div className="message items-center flex mb-5 flex-col">
+        <a
+          className="p-2 w-55 text-center font-bold bg-purple-800 text-amber-50 rounded-xl"
+          href="/"
+        >
+          Register
+        </a>
+      </div>
     </div>
   )
 }
--- Frontend/src/components/Register/SignUp/Doctor.jsx
+++ Frontend/src/components/Register/SignUp/Doctor.jsx
@@ -2,39 +2,44 @@ import React from 'react'
 import Signupnav from './Signupnav'
 const Doctor = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center mb-10'>
       <Signupnav/>
-      <div className="name">
-        <input type="text" placeholder='First Name' />
-        <input type="text" placeholder='Last Name' />
+
+      <div className="heading text-center mb-5 ">
+        <h1 className="font-bold text-4xl">Sign Up</h1>
       </div>
 
-      <div className="details">
-        <input type="email" placeholder='Email' />
-        <input type="number" placeholder='Mobile Number' />
+      <div className="name mb-5">
+        <input type="text"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='First Name' />
+        <input type="text"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Last Name' />
       </div>
 
-      <div className="department">
-        <select name="" id="Departments">
+      <div className="details mb-5">
+        <input type="email"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Email' />
+        <input type="number"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Mobile Number' />
+      </div>
+
+      <div className="department mb-5">
+        <select name=""  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" id="Departments">
             <option value="Departments"> Department Name </option>
         </select>
-        <input type="number" placeholder='Adhar card Number' />
+        <input type="number"  className="border-4 border-gray-500 rounded-lg p-2 w-100 mr-4" placeholder='Adhar card Number' />
       </div>
 
-      <div className="password">
-        <select name="" id="gender">
+      <div className="password mb-5">
+        <select name=""  className="border-4 border-gray-500 rounded-lg p-2 w-65 mr-4" id="gender">
             <option value="gender">Gender</option>
             <option value="gender">Male</option>
             <option value="gender">Female</option>
             <option value="gender">Other</option>
         </select>
 
-        <input type="password" placeholder='Password' id='password' />
-        <input type="password" placeholder='Confirm Password' id='password' />
+        <input type="password"  className="border-4 border-gray-500 rounded-lg p-2 w-65 mr-4" placeholder='Password' id='password' />
+        <input type="password"  className="border-4 border-gray-500 rounded-lg p-2 w-65 mr-4" placeholder='Confirm Password' id='password' />
       </div>
 
       <div className="button">
-        <a href="http://" className='p-2 w-55 text-center font-bold bg-purple-800 text-amber-50 rounded-xl' >Register</a>
+        <a href="http://" className='p-2  text-center font-bold bg-purple-800 text-amber-50 rounded-xl w-[10%]' >Register</a>
       </div>
     </div>
   )
